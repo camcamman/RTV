ff0480a8387a0fa0787ec5bd6680d043e0ceec7f
Cameron Ford <c.r.ford@outlook.com>
2023-03-10 15:04:37 -0700

    fixed return on all crud oporations

--- routes/comments.js
+++ routes/comments.js
@@ -6,31 +6,31 @@ const commentsDb = require("../models/CommentsRouter")
 //get all 
 commentsRouter.get("/", async (req, res, next)=> {
     const stuff = await commentsDb.find({}).exec()
-    return res.status(202).send(stuff);
+    return res.status(200).send(stuff);
 })
 
 //get one 
 commentsRouter.get("/:commentsId", async (req, res, next) => {
     const foundComment = await commentsDb.findOne({}).exec()
-    return foundComment
+    return res.status(200).send(foundComment)
 })
 
 //add one 
 commentsRouter.post("/", async (req, res, next) => {
     const addedComment = await commentsDb.save({}).exec()
-    return addedComment
+    return res.status(200).send(addedComment)
 })
 
 //delete one 
 commentsRouter.delete("/:commentsId", async (req, res) => {
     const deletedComment = await commentsDb.findOneAndDelete({}).exec()
-    return deletedComment 
+    return res.status(200).send(deletedComment)
 })
 
 //edit One 
 commentsRouter.put("/:commentsId", async (req, res) => {
     const updatedComment = await commentsDb.findOneAndUpdate({}).exec()
-    return updatedComment
+    return res.status(200).send(updatedComment) 
 })
 
 
