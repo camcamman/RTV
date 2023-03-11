2f15d23c2c22f3b77e218e9851859c7f31ef7d31
Cameron Ford <c.r.ford@outlook.com>
2023-03-11 16:11:26 -0700

    added signup route. no login yet

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,7 +3,23 @@ const authRouter = express.Router()
 const User = require('../models/user.js')
 const jwt = require('jsonwebtoken')
 
-authRouter.post("./signup", (req, res, next) => {})
+//signup
+authRouter.post("/signup", async (req, res, next) => {
+    const foundUsername = await User.findOne({ username: req.body.username.toLowerCase() }).exec()
+
+    if(foundUsername !== null){
+        res.status(403)
+        return next(new Error("That username is already taken"))
+    }
+
+    const newUser = new User(req.body)
+
+    const saveNewUser = await newUser.save()
+
+    const token = jwt.sign(saveNewUser.toObject(), process.env.SECRET)
+
+    return res.status(201).send({ token, user: saveNewUser})
+})
 
 
 module.exports = authRouter
