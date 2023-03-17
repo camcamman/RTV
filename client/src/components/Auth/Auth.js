0c56b98ac7fe9696c4584c19296b1a6ce6c82b0a
Cameron Ford <c.r.ford@outlook.com>
2023-03-17 03:02:47 -0600

    fixed inputs. now inputs working with state

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,18 +7,29 @@ export default function Home () {
     const [inputs, setInputs] = useState(initInputs)
     const [toggle, setToggle] = useState(true)
 
+    function handleChange(e){
+        const {name, value} = e.target
+        setInputs(prevInputs => ({
+          ...prevInputs,
+          [name]: value
+        }))
+        console.log(inputs)
+    }
 
-    function handleChange () {}
     return(
         <div>
             {toggle ?
             <>
-            <AuthForm/>
+            <AuthForm
+                handleChange={handleChange}
+            />
             <button onClick={() => setToggle(prev => !prev)}>Already a member?</button>
             </> 
             :
             <>
-            <AuthForm/>
+            <AuthForm
+                handleChange={handleChange}
+            />
           <button onClick={() => setToggle(prev => !prev)}>Not a member?</button>
 
             </>
--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -1,18 +1,25 @@
 import React from "react";
 
-export default function AuthForm () {
+export default function AuthForm (props) {
+
+    const { handleChange } = props
+
     return(
         <>
             <form>
                 <input
                     type="text"
                     placeholder="username"
+                    onChange={handleChange}
+                    name="username"
                 >
                 </input>
 
                 <input
                     type="text"
                     placeholder="password"
+                    onChange={handleChange}
+                    name="password"
                 >
                 </input>
             </form>
